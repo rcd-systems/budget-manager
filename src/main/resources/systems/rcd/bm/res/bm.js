b25e87aa642311d230740c94fea5bff7efa6646d
RcdSystems <git.rcd.systems@gmail.com>
2016-01-16 18:38:06 +0100

    Refact

--- src/main/resources/systems/rcd/bm/res/bm.js
+++ src/main/resources/systems/rcd/bm/res/bm.js
@@ -1,4 +1,37 @@
-$("#bm-nav-button-transfers").click(function(){
-	$("#bm-section-home").addClass("hidden");
-	$("#bm-section-transfer").removeClass("hidden");
+var currentSectionSelector = "#bm-section-home";
+
+function display(selector) {
+	$(selector).removeClass("hidden");
+}
+
+function hide(selector) {
+	$(selector).addClass("hidden");
+}
+
+function displaySection(selector) {
+	hide(currentSectionSelector);
+	display(selector);
+	currentSectionSelector = selector;
+}
+
+function displaySectionTransfers() {
+	displaySection("#bm-section-transfers");
+	
+	$.ajax({
+	  url: "test.html",
+	  dataType: "html",
+	  success: (data) => {
+		  $("#bm-section-transfers").html(data);
+	  }
+	});
+}
+
+
+
+
+$("#bm-nav-button-transfers").click(function(){	
+	displaySectionTransfers();
 });
+
+
+
