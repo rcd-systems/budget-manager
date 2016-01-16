5347b8021b3612e85fa8ed4d60ea5b7e611484cb
RcdSystems <git.rcd.systems@gmail.com>
2016-01-16 10:29:24 +0100

    Server launch + interface skeleton

--- src/main/java/systems/rcd/bm/Main.java
+++ src/main/java/systems/rcd/bm/Main.java
@@ -3,6 +3,7 @@ package systems.rcd.bm;
 import systems.rcd.bm.model.BmModelService;
 import systems.rcd.fwk.core.ctx.RcdContext;
 import systems.rcd.fwk.core.log.RcdLogService;
+import systems.rcd.fwk.jetty.impl.RcdJettyService;
 import systems.rcd.fwk.poi.xls.impl.RcdPoiXlsService;
 
 public class Main {
@@ -10,15 +11,24 @@ public class Main {
     public static void main(final String[] args) throws Exception {
         RcdLogService.info("Budget Manager 8");
         configure();
-        load();
+        loadData();
+        launchServer();
+
     }
 
     private static void configure() {
         RcdPoiXlsService.init();
     }
 
-    private static void load() throws Exception {
+    private static void loadData() throws Exception {
         final BmModelService bmModelService = new BmModelService();
         RcdContext.setGlobalServiceSupplier(BmModelService.class, () -> bmModelService);
+    }
+
+    private static void launchServer() throws Exception {
+        RcdJettyService.createServer(0)
+                .addResourceHandler("/res", Main.class.getResource("res"))
+        .start()
+        .join();
     }
 }
